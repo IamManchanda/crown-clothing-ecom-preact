c6e1fe5bac6d3fa2ec5e565bba363a3005d46c04
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-06 00:41:22 +0530

    Conditionally render "Your cart is empty" if it is

--- src/components/cart-dropdown/index.js
+++ src/components/cart-dropdown/index.js
@@ -10,9 +10,13 @@ import "./styles.scss";
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty.</span>
+      )}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
