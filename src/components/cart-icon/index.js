c3d8293e118dc7d6e5be87a204da07e0a76f4424
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-05 22:57:10 +0530

    Item count in Cart!

--- src/components/cart-icon/index.js
+++ src/components/cart-icon/index.js
@@ -5,14 +5,20 @@ import { toggleCartHidden } from "../../store/cart/cart.actions";
 import "./styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
-const mapStateToProps = null;
+const mapStateToProps = ({ cart: { cartItems } = {} }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedItemCount, cartItem) =>
+      accumulatedItemCount + cartItem.quantity,
+    0,
+  ),
+});
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
