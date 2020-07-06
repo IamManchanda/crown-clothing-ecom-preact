1f6e83c0777e1ffbdc4936bf37c4dbae0bd21a97
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-06 16:12:45 +0530

    Hide cart dropdown when checkout page is visited from cart

--- src/components/cart-dropdown/index.js
+++ src/components/cart-dropdown/index.js
@@ -6,9 +6,10 @@ import { withRouter } from "react-router-dom";
 import CustomButton from "../custom-button";
 import CartItem from "../cart-item";
 import { selectCartItems } from "../../store/cart/cart.selectors";
+import { toggleCartHidden } from "../../store/cart/cart.actions";
 import "./styles.scss";
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -19,7 +20,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty.</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push("/checkout")}>
+    <CustomButton
+      onClick={() => {
+        history.push("/checkout");
+        dispatch(toggleCartHidden());
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
