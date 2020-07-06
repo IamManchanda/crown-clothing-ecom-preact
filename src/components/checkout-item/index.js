701789a1b23275ec9d7b820b6f965a3226162c34
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-06 19:08:28 +0530

    Increase decrease markup & styling!

--- src/components/checkout-item/index.js
+++ src/components/checkout-item/index.js
@@ -13,7 +13,11 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt={name} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow">&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow">&#10095;</div>
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
