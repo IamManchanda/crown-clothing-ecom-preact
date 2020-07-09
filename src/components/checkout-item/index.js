b7299c4cb1fefcfd8184ce7c7ca8c16b093c5415
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-09 21:33:33 +0530

    Use CSS-in-JS within Checkout Item

--- src/components/checkout-item/index.js
+++ src/components/checkout-item/index.js
@@ -2,35 +2,43 @@ import React from "react";
 import { connect } from "react-redux";
 
 import {
+  CheckoutItemStyled,
+  ImageContainerStyled,
+  NameStyled,
+  QuantityStyled,
+  QuantityArrowStyled,
+  QuantityValueStyled,
+  PriceStyled,
+  RemoveButtonStyled,
+} from "./styles";
+import {
   clearItemFromCart,
   addItem,
   removeItem,
 } from "../../store/cart/cart.actions";
 
-import "./styles.scss";
-
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, price, imageUrl, quantity } = cartItem;
   return (
-    <div className="checkout-item">
-      <div className="image-container">
+    <CheckoutItemStyled>
+      <ImageContainerStyled>
         <img src={imageUrl} alt={name} />
-      </div>
-      <span className="name">{name}</span>
-      <span className="quantity">
-        <div className="arrow" onClick={() => removeItem(cartItem)}>
+      </ImageContainerStyled>
+      <NameStyled>{name}</NameStyled>
+      <QuantityStyled>
+        <QuantityArrowStyled onClick={() => removeItem(cartItem)}>
           &#10094;
-        </div>
-        <span className="value">{quantity}</span>
-        <div className="arrow" onClick={() => addItem(cartItem)}>
+        </QuantityArrowStyled>
+        <QuantityValueStyled>{quantity}</QuantityValueStyled>
+        <QuantityArrowStyled onClick={() => addItem(cartItem)}>
           &#10095;
-        </div>
-      </span>
-      <span className="price">{price}</span>
-      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        </QuantityArrowStyled>
+      </QuantityStyled>
+      <PriceStyled>{price}</PriceStyled>
+      <RemoveButtonStyled onClick={() => clearItem(cartItem)}>
         &#10005;
-      </div>
-    </div>
+      </RemoveButtonStyled>
+    </CheckoutItemStyled>
   );
 };
 
