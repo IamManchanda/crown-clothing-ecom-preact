eb46c6e8d9986247bc5ad5065693bb226e268192
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-09 19:16:39 +0530

    Remove unused styles.scss file!

--- src/pages/shop-page/index.js
+++ src/pages/shop-page/index.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Route } from "react-router-dom";
 
-import "./styles.scss";
 import ShopPageCollection from "./_collection";
 import CollectionsOverview from "../../components/collections-overview";
 
 const ShopPage = ({ match }) => (
-  <div className="shop-page">
+  <Fragment>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collection`} component={ShopPageCollection} />
-  </div>
+  </Fragment>
 );
 
 export default ShopPage;
