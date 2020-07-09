3aaea99d31da5beae23cb8e87838a48c76c5e626
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-09 20:37:48 +0530

    Use CSS-in-JS within Cart icon

--- src/components/cart-icon/index.js
+++ src/components/cart-icon/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import { CartIconStyled, ShoppingIconStyled, ItemCountStyled } from "./styles";
 import { toggleCartHidden } from "../../store/cart/cart.actions";
 import { selectCartItemsCount } from "../../store/cart/cart.selectors";
-import "./styles.scss";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">{itemCount}</span>
-  </div>
+  <CartIconStyled onClick={toggleCartHidden}>
+    <ShoppingIconStyled />
+    <ItemCountStyled>{itemCount}</ItemCountStyled>
+  </CartIconStyled>
 );
 
 const mapStateToProps = createStructuredSelector({
