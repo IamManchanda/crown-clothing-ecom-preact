5d1a0353b7d82559983e74e4025a0d42e28181f2
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-09 20:30:20 +0530

    Use CSS-in-JS within Collection preview

---
+++ src/components/collection-preview/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const CollectionPreviewStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+`;
+
+export const TitleStyled = styled.div`
+  font-size: 28px;
+  margin-bottom: 25px;
+`;
+
+export const PreviewStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FooterStyled = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 25px;
+`;
