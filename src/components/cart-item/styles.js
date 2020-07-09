b70f5eee9c89b77dc3f551097abb6845e2873ca2
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-09 20:48:18 +0530

    Use CSS-in-JS in Cart item

---
+++ src/components/cart-item/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const CartItemStyled = styled.div`
+  width: 100%;
+  display: flex;
+  height: 80px;
+  margin-bottom: 15px;
+
+  img {
+    width: 30%;
+  }
+`;
+
+export const ItemDetailsStyled = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 10px 20px;
+`;
+
+export const ItemNameStyled = styled.span`
+  font-size: 16px;
+`;
