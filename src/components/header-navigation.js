86219a3d6b4f8d97bb51f84cc355dacab03333c7
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-10 15:56:12 +0530

    Refactor header navigation component

--- src/components/header-navigation.js
+++ src/components/header-navigation.js
@@ -1,19 +1,42 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-import {
-  HeaderNavigationStyled,
-  LogoContainerStyled,
-  OptionsContainerStyled,
-  OptionLinkStyled,
-} from "./styles";
-import { ReactComponent as Logo } from "../../assets/svg/crown.svg";
-import { auth } from "../../firebase/utils";
-import CartIcon from "../cart-icon";
-import CartDropdown from "../cart-dropdown";
-import { selectCurrentUser } from "../../store/user/user.selectors";
-import { selectCartHidden } from "../../store/cart/cart.selectors";
+import { ReactComponent as Logo } from "../assets/svg/crown.svg";
+import { auth } from "../firebase/utils";
+import CartIcon from "./cart-icon";
+import CartDropdown from "./cart-dropdown";
+import { selectCurrentUser } from "../store/user/user.selectors";
+import { selectCartHidden } from "../store/cart/cart.selectors";
+
+const HeaderNavigationStyled = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`;
+
+const LogoContainerStyled = styled(Link)`
+  height: 100%;
+  width: 70px;
+  padding: 10px;
+`;
+
+const OptionsContainerStyled = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const OptionLinkStyled = styled(Link)`
+  padding: 10px 15px;
+  cursor: pointer;
+`;
 
 const HeaderNavigation = ({ currentUser, hidden }) => (
   <HeaderNavigationStyled>
