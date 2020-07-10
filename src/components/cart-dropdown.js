3d7e6bcad9f084525086f2a63d180a1cc79c0e40
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-10 14:49:01 +0530

    Refactor Cart dropdown

--- src/components/cart-dropdown.js
+++ src/components/cart-dropdown.js
@@ -2,16 +2,42 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { withRouter } from "react-router-dom";
+import styled from "styled-components";
 
-import {
-  CartDropdownStyled,
-  GoToCheckoutButtonStyled,
-  EmptyMessageStyled,
-  CartItemsContainerStyled,
-} from "./styles";
-import CartItem from "../cart-item";
-import { selectCartItems } from "../../store/cart/cart.selectors";
-import { toggleCartHidden } from "../../store/cart/cart.actions";
+import CustomButton from "./custom-button";
+import CartItem from "./cart-item";
+import { selectCartItems } from "../store/cart/cart.selectors";
+import { toggleCartHidden } from "../store/cart/cart.actions";
+
+const CartDropdownStyled = styled.div`
+  position: absolute;
+  width: 240px;
+  height: 340px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  top: 90px;
+  right: 50px;
+  z-index: 5;
+`;
+
+const GoToCheckoutButtonStyled = styled(CustomButton)`
+  margin-top: auto;
+`;
+
+const EmptyMessageStyled = styled.span`
+  font-size: 18px;
+  margin: 50px auto;
+`;
+
+const CartItemsContainerStyled = styled.div`
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+`;
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
   <CartDropdownStyled>
