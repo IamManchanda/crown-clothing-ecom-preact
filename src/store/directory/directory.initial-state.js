53bf6be3ec01a1ffd52d7166bccdd023041c7d57
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-10 16:07:22 +0530

    Move temporary data into fixtures

--- src/store/directory/directory.initial-state.js
+++ src/store/directory/directory.initial-state.js
@@ -1,40 +1,7 @@
-import { v4 as uuidv4 } from "uuid";
+import DIRECTORY_SECTIONS from "../../fixtures/directory-sections";
 
 const DIRECTORY_INITIAL_STATE = {
-  sections: [
-    {
-      id: uuidv4(),
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-      linkUrl: "shop/hats",
-    },
-    {
-      id: uuidv4(),
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      linkUrl: "shop/sneakers",
-    },
-    {
-      id: uuidv4(),
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-      linkUrl: "shop/jackets",
-    },
-    {
-      id: uuidv4(),
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      size: "large",
-      linkUrl: "shop/womens",
-    },
-    {
-      id: uuidv4(),
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      size: "large",
-      linkUrl: "shop/mens",
-    },
-  ],
+  sections: [...DIRECTORY_SECTIONS],
 };
 
 export default DIRECTORY_INITIAL_STATE;
