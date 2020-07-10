ac571006569e080d939d5b29a2482ef753dc1a06
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-10 14:58:51 +0530

    Refactor Cart item & checkout item!

--- src/components/checkout-item.js
+++ src/components/checkout-item.js
@@ -1,21 +1,63 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import {
-  CheckoutItemStyled,
-  ImageContainerStyled,
-  NameStyled,
-  QuantityStyled,
-  QuantityArrowStyled,
-  QuantityValueStyled,
-  PriceStyled,
-  RemoveButtonStyled,
-} from "./styles";
+import styled, { css } from "styled-components";
+
 import {
   clearItemFromCart,
   addItem,
   removeItem,
-} from "../../store/cart/cart.actions";
+} from "../store/cart/cart.actions";
+
+const CheckoutItemStyled = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 1px solid darkgrey;
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+`;
+
+const CheckoutColumnStyled = css`
+  width: 23%;
+`;
+
+const ImageContainerStyled = styled.div`
+  ${CheckoutColumnStyled}
+  padding-right: 15px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const NameStyled = styled.span`
+  ${CheckoutColumnStyled}
+`;
+
+const QuantityStyled = styled.span`
+  ${CheckoutColumnStyled}
+  display: flex;
+`;
+
+const QuantityArrowStyled = styled.div`
+  cursor: pointer;
+`;
+
+const QuantityValueStyled = styled.span`
+  margin: 0 10px;
+`;
+
+const PriceStyled = styled.span`
+  ${CheckoutColumnStyled}
+`;
+
+const RemoveButtonStyled = styled.div`
+  padding-left: 12px;
+  cursor: pointer;
+`;
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, price, imageUrl, quantity } = cartItem;
