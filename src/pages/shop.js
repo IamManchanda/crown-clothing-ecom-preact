f0e986aab4052dae9375b0a69c28ea2b3517ac27
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-11 18:21:51 +0530

    Folder restructuring: Shop Page

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -2,9 +2,9 @@ import React, { Fragment, Component } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import { fetchCollectionsStartAsync } from "../../store/shop/shop.actions";
-import CollectionsOverviewWithContainer from "../../components/collections-overview/with-container";
-import CollectionPageWithContainer from "../../components/collection-page/with-container";
+import { fetchCollectionsStartAsync } from "../store/shop/shop.actions";
+import CollectionsOverviewWithContainer from "../components/collections-overview/with-container";
+import CollectionPageWithContainer from "../components/collection-page/with-container";
 
 class ShopPage extends Component {
   componentDidMount() {
