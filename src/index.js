d83a855a583ca65f1c07220c398905cc820a21ec
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-11 22:01:20 +0530

    Add back boilerplate serviceWorker code for upcominhg use.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+import * as serviceWorker from "./serviceWorker";
 import "./assets/scss/global-styles.scss";
 import App from "./app";
 import { store, persistor } from "./store";
@@ -20,3 +21,8 @@ render(
   </StrictMode>,
   document.getElementById("root"),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
