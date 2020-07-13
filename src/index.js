2fb055768fd8e6f79a9d692b4db4000cdaf7c5f0
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-14 01:54:15 +0530

    Revert "React Snapshot"
    This reverts commit 6992f023368ef4638d698e6296c64a3d287d3427.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { StrictMode } from "react";
-import { render } from "react-snapshot";
+import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
