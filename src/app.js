5b5520b1aa3e75a322f802cf90066bb55e0cdf4c
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-07-15 13:40:57 +0530

    Environment update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { h, Component } from "preact";
+import { h } from "preact";
 import { Router } from "preact-router";
 
 import Header from "./components/header";
@@ -8,26 +8,18 @@ import Home from "./routes/home";
 import About from "./routes/about";
 import Shop from "./routes/shop";
 
-export default class App extends Component {
-  /** Gets fired when the route changes.
-   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-   *	@param {string} event.url	The newly routed URL
-   */
-  handleRoute = (e) => {
-    this.currentUrl = e.url;
-  };
+const App = () => {
+  return (
+    <div id="app">
+      <Header />
+      <Router>
+        <Home path="/" />
+        <About path="/about" />
+        <Shop path="/shop/" />
+        <Shop path="/shop/:collectionId" />
+      </Router>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div id="app">
-        <Header />
-        <Router onChange={this.handleRoute}>
-          <Home path="/" />
-          <About path="/about" />
-          <Shop path="/shop/" />
-          <Shop path="/shop/:collectionId" />
-        </Router>
-      </div>
-    );
-  }
-}
+export default App;
--- src/routes/shop.js
+++ src/routes/shop.js
@@ -1,23 +1,16 @@
-import { h, Component } from "preact";
+import { h } from "preact";
 import { Router } from "preact-router";
 import CollectionsOverview from "../components/collections-overview";
 import CollectionPage from "../components/collection-page";
 
-export default class Shop extends Component {
-  handleRoute = (e) => {
-    this.currentUrl = e.url;
-  };
+const Shop = ({ collectionId }) => (
+  <Router>
+    <CollectionsOverview path="/shop" />
+    <CollectionPage
+      collectionId={collectionId}
+      path={`/shop/:${collectionId}`}
+    />
+  </Router>
+);
 
-  render() {
-    const { props: { collectionId } = {} } = this;
-    return (
-      <Router onChange={this.handleRoute}>
-        <CollectionsOverview path="/shop" />
-        <CollectionPage
-          collectionId={collectionId}
-          path={`/shop/:${collectionId}`}
-        />
-      </Router>
-    );
-  }
-}
+export default Shop;
